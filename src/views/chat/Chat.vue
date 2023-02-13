50734529666f59c258faa01fa27c9f9713f846a8
Àlex Gómez <alexalbons@hotmail.com>
2023-02-13 10:07:37 +0100

    Chat dinamico

--- src/views/chat/Chat.vue
+++ src/views/chat/Chat.vue
@@ -2,8 +2,6 @@
   <div class="flex w-full h-14 items-center justify-start">
     <h2 class="text-lg font-medium mr-auto w-1/4">Chat</h2>
     <div class="flex w-full">
-      <h2 id="group-name">
-      </h2>
       <div class="flex justify-between w-full">
         <input type="text" name="username" id="username" placeholder="Usuari" />
         <button type="submit" class="box mr-4 p-3" v-on:click="createUser()">
@@ -15,20 +13,20 @@
   <div id="app" class="flex">
     <div id="group-list" class="flex flex-col gap-5 w-1/4">
       <button
+        v-for="group in allGroups.data"
         class="flex p-3 box h-24 w-[17rem] cursor-pointer"
-        @click="loadGroup('supergroup')"
+        @click="loadGroup(group.guid)"
       >
-        <b>Super</b>
+        <b>{{ group.name }}</b>
       </button>
-      <button
-        class="flex p-3 box h-24 w-[17rem] cursor-pointer"
-        @click="loadGroup('global')"
-      >
-        <b>Global</b>
-      </button>
-      <!-- <button v-for="prop in props">{{ prop }}</button> -->
     </div>
     <div class="flex flex-col w-full h-[85vh] justify-between items-center box">
+      <div class="flex items-center border border-1 h-20 w-full">
+        <div
+          id="current-chat"
+          class="flex gap-5 items-center justify-center h-14 p-2"
+        ></div>
+      </div>
       <div
         id="messages"
         class="flex flex-col gap-2 h-5/6 w-full p-5 overflow-y-scroll"
@@ -59,15 +57,18 @@
 
 <script setup>
 import { CometChat } from "@cometchat-pro/chat";
+import { ref } from "vue";
 
 // Credenciales de la Aplicacion de CometChat
 const appID = "23195116ca7e245f";
 const region = "eu";
 
+// Variables globales
 let userData;
 let actualGroup;
-let allGroups;
+let allGroups = ref("");
 
+// Variable de configuración del chat
 const appSetting = new CometChat.AppSettingsBuilder()
   .subscribePresenceForAllUsers()
   .setRegion(region)
@@ -83,30 +84,7 @@ CometChat.init(appID, appSetting).then(
   }
 );
 
-// Funció per rebre la llista de grups
-const getGroupsList = () => {
-  const limit = 30;
-  const groupsRequest = new CometChat.GroupsRequestBuilder()
-    .setLimit(limit)
-    .build();
-
-  groupsRequest.fetchNext().then(
-    (groupList) => {
-      console.log("Lista de grupos:", groupList);
-      // groupList.map((group) => {
-      //   document.getElementById(
-      //     "group-list"
-      //   ).innerHTML += `<button class="flex p-3 box h-24 w-[17rem] cursor-pointer" @click="loadGroup('${group.guid}')">
-      //       <b>${group.name}</b>
-      //     </button>`;
-      // });
-    },
-    (error) => {
-      console.log("Error al obtener la lista de grupos:", error);
-    }
-  );
-};
-
+// Función para crear un usuario en caso de que el nuevo no exista
 const createUser = () => {
   const username = document.getElementById("username").value;
 
@@ -131,11 +109,12 @@ const createUser = () => {
   );
 };
 
+// Función para loguear un usuario
 const logUserIn = (authKey, UID) => {
-  getGroupsList();
   CometChat.login(UID, authKey).then(
     (user) => {
       console.log("Login Successful:", { user });
+      getGroupsList(user.uid);
     },
     (error) => {
       console.log("Login failed with exception:", { error });
@@ -143,6 +122,32 @@ const logUserIn = (authKey, UID) => {
   );
 };
 
+// Función para obtener la lista de grupos del usuario en cuestión
+const getGroupsList = (uid) => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      onBehalfOf: uid,
+      apikey: "a76fca94f28c87446b3de6dd7785355fbc9d8d78",
+    },
+  };
+
+  fetch("https://23195116ca7e245f.api-eu.cometchat.io/v3/groups", options)
+    .then((response) => response.json())
+    .then((response) => {
+      allGroups.value = response;
+      console.log(allGroups);
+      let temp = "";
+      response.data.map((groups) => {
+        temp += `<button class="flex p-3 box h-24 w-[17rem] cursor-pointer" v-on:click="loadGroup('${groups.guid}')">${groups.name}</button>`;
+        document.getElementById("prova").innerHTML = temp;
+      });
+    })
+    .catch((err) => console.error(err));
+};
+
+// Función para entrar al grupo y cargar los mensajes del mismo
 const loadGroup = (GID) => {
   const user = userData;
 
@@ -152,21 +157,23 @@ const loadGroup = (GID) => {
 
   actualGroup = GID;
 
+  // Llamamos a la función de cargar el historial de mensajes del grupo en cuestión
   getMessageHistory(user, GUID);
+
+  // Llamamos a la función para preparar un listener de recibir y enviar mensajes
   createMsgListener();
 
   CometChat.joinGroup(GUID, groupType, password).then(
     (group) => {
       console.log("Se ha accedido al grupo:", group);
-      document.getElementById("group-name").innerHTML = group.name;
     },
     (error) => {
       console.log("Error al entrar al grupo:", error);
-      // document.getElementById("group-name").innerHTML = group.name;
     }
   );
 };
 
+// Función para recibir mensajes
 const createMsgListener = () => {
   const listenerID = "GLOBAL_LISTENER_ID";
 
@@ -189,6 +196,22 @@ const createMsgListener = () => {
   );
 };
 
+// Convertir la hora del formato numerico a fecha
+const convertStringToDate = (strTime) => {
+  const timestamp = Number(strTime) * 1000;
+  const date = new Date(timestamp);
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  // const ampm = hours >= 12 ? "pm" : "am";
+  // hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var timeStr = hours + ":" + minutes;
+
+  return timeStr.toString();
+};
+
+// Obtener la lista de mensajes de un grupo
 const getMessageHistory = (user, GID) => {
   const GUID = GID;
   const limit = 30;
@@ -203,16 +226,51 @@ const getMessageHistory = (user, GID) => {
     (messages) => {
       messages = messages.filter((message) => message.type === "text");
       console.log("Mensajes anteriores: ", messages);
+
+      document.getElementById(
+        "current-chat"
+      ).innerHTML = `<img src="${messages[0].receiver.getIcon()}" class="w-12 h-12" />`;
+      if (messages[0].receiver.getIcon() === undefined) {
+        document.getElementById(
+          "current-chat"
+        ).innerHTML = `<img class="rounded-full" src="https://ui-avatars.com/api/?name=${messages[0].receiver.name.charAt(
+          0
+        )}&color=7F9CF5&background=EBF4FF" class="w-12 h-12" />`;
+      }
+      document.getElementById(
+        "current-chat"
+      ).innerHTML += `<h2 class="w-full font-bold text-2xl">${messages[0].receiver.name}</h2>`;
       messages.map((message) => {
         if (message.text !== undefined) {
           if (user.name === message.sender.name) {
             document.getElementById(
               "messages"
-            ).innerHTML += `<div class="message flex gap-1 justify-end"><p class="py-2 px-7 bg-blue-600 rounded-3xl text-white">${message.text}</p></div>`;
+            ).innerHTML += `<div class="message flex gap-1 justify-end">
+                              <div class="flex gap-3 py-2 px-7 bg-[#8DF7F2] rounded-lg max-w-md">
+                                <p>
+                                  ${message.text}
+                                </p>
+                                <div class="flex flex-col justify-end items-center">
+                                  <p class="text-gray-400">${convertStringToDate(
+                                    message.sentAt
+                                  )}</p>
+                                </div>
+                              </div>
+                            </div>`;
           } else {
             document.getElementById(
               "messages"
-            ).innerHTML += `<div class="message flex flex-col gap-1 rounded-3xl w-fit py-2 px-5 bg-gray-200"><b>${message.sender.name}</b><p>${message.text}</p></div>`;
+            ).innerHTML += `<div class="message flex gap-3 rounded-lg w-fit py-2 px-5 bg-gray-200 max-w-md">
+                              <div class="flex flex-col">
+                                <b>${message.sender.name}</b>
+                                <p>${message.text}</p>
+                              </div>
+                              <div class="flex flex-col justify-end items-center">
+                                <p class="text-gray-400">${convertStringToDate(
+                                  message.sentAt
+                                )}</p>
+                              </div>
+                            </div>`;
           }
         }
       });
@@ -223,6 +281,7 @@ const getMessageHistory = (user, GID) => {
   );
 };
 
+// Función para mandar mensajes
 const sendMessage = () => {
   const receiverID = actualGroup;
   const messageText = document.getElementById("message").value;
@@ -240,7 +299,18 @@ const sendMessage = () => {
     (message) => {
       document.getElementById(
         "messages"
-      ).innerHTML += `<div class="message flex gap-1 justify-end"><p class="py-2 px-7 bg-blue-600 rounded-3xl text-white">${message.text}</p></div>`;
+      ).innerHTML += `<div class="message flex gap-1 justify-end">
+                              <div class="flex gap-3 py-2 px-7 bg-[#8DF7F2] rounded-lg max-w-md">
+                                <p>
+                                  ${message.text}
+                                </p>
+                                <div class="flex flex-col justify-end items-center">
+                                  <p class="text-gray-400">${convertStringToDate(
+                                    message.sentAt
+                                  )}</p>
+                                </div>
+                              </div>
+                            </div>`;
       console.log("Mensaje enviado correctamente:", message);
     },
     (error) => {
@@ -249,18 +319,4 @@ const sendMessage = () => {
   );
 };
 
-const props = [
-  {
-    nom: "jordi",
-    edat: 43,
-  },
-  {
-    nom: "pere",
-    edat: 52,
-  },
-  {
-    nom: "alex",
-    edat: 23,
-  },
-];
 </script>
