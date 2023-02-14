93d57344844a761469e80de3bc4d773fe1a43879
Àlex Gómez <alexalbons@hotmail.com>
2023-02-14 13:48:15 +0100

    Creacion de grupos y chats

--- src/views/chat/Chat.vue
+++ src/views/chat/Chat.vue
@@ -1,35 +1,97 @@
 <template>
+  <div
+    id="new-chat-modal"
+    class="hidden items-center justify-center absolute z-10 top-0 bottom-80 left-0 right-0"
+  >
+    <div
+      class="flex flex-col bg-gray-200 box border border-gray-100 items-center gap-5 w-64 pb-5"
+    >
+      <div class="flex w-full p-3 items-center justify-between">
+        <p class="text-xl">Crear nuevo {{ chatType }}</p>
+        <div
+          class="cursor-pointer text-black font-bold text-2xl"
+          @click="toggleModal()"
+        >
+          X
+        </div>
+      </div>
+      <div class="flex flex-col gap-5">
+        <input
+          v-if="chatType === 'grupo'"
+          id="new-chat-input"
+          type="text"
+          :placeholder="'Nombre del grupo...'"
+        />
+        <select
+          name="newChatSelect"
+          id="new-chat-select"
+          :multiple="chatType === 'grupo' ? true : false"
+          class="w-48"
+        >
+          <option v-for="user in usersList" :value="user.uid">
+            {{ user.name }}
+          </option>
+        </select>
+      </div>
+      <button class="box p-2 font-bold" @click="createNewChat()">Crear</button>
+    </div>
+  </div>
   <div class="flex w-full h-14 items-center justify-start">
     <h2 class="text-lg font-medium mr-auto w-1/4">Chat</h2>
     <div class="flex w-full">
       <div class="flex justify-between w-full">
         <input type="text" name="username" id="username" placeholder="Usuari" />
-        <button type="submit" class="box mr-4 p-3" v-on:click="createUser()">
-          Accedir
-        </button>
+        <div class="flex gap-1">
+          <button type="submit" class="box mr-4 p-3" @click="createUser()">
+            Accedir
+          </button>
+          <button class="box w-[2.5rem] text-4xl" @click="toggleMenu()">
+            ⋮
+          </button>
+          <div
+            id="new-chat-menu"
+            class="hidden flex-col w-60 bg-white absolute z-10 right-6 top-12 border border-gray-100 rounded"
+          >
+            <div
+              class="pl-5 py-2 w-full cursor-pointer border border-bottom hover:bg-gray-200"
+              @click="newChat('grupo')"
+            >
+              <p>Nuevo grupo</p>
+            </div>
+            <div
+              class="pl-5 py-2 w-full cursor-pointer border border-bottom hover:bg-gray-200"
+              @click="newChat('chat')"
+            >
+              <p>Nuevo chat</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
   <div id="app" class="flex">
     <div id="group-list" class="flex flex-col gap-5 w-1/4">
       <button
-        v-for="group in allGroups.data"
-        class="flex gap-5 p-3 box h-24 w-[17rem] cursor-pointer"
-        @click="loadGroup(group.guid)"
+        v-for="group in allGroups"
+        class="flex gap-3 p-3 box h-24 w-[17rem] cursor-pointer"
+        @click="loadGroup(group.conversationWith.guid)"
       >
         <div class="h-full w-14 flex items-center justify-center">
           <img
             :src="
-              group.icon
-                ? group.icon
-                : `https://ui-avatars.com/api/?name=${group.name.charAt(
+              group.conversationWith.icon
+                ? group.conversationWith.icon
+                : `https://ui-avatars.com/api/?name=${group.conversationWith.name.charAt(
                     0
                   )}&color=7F9CF5&background=EBF4FF`
             "
             class="w-14 h-14 rounded-full"
           />
         </div>
-        <b>{{ group.name }}</b>
+        <div class="flex flex-col justify-between h-full">
+          <b>{{ group.conversationWith.name }}</b>
+          <p :id="'last-message-' + group.conversationWith.guid"></p>
+        </div>
       </button>
     </div>
     <div class="flex flex-col w-full h-[85vh] justify-between items-center box">
@@ -79,6 +141,9 @@ const region = "eu";
 let userData;
 let actualGroup;
 let allGroups = ref("");
+let chatType = ref("");
+let chatTag = ref("");
+let usersList = ref("");
 
 // Variable de configuración del chat
 const appSetting = new CometChat.AppSettingsBuilder()
@@ -126,7 +191,8 @@ const logUserIn = (authKey, UID) => {
   CometChat.login(UID, authKey).then(
     (user) => {
       console.log("Login Successful:", { user });
-      getGroupsList(user.uid);
+      // getGroupsList(user.uid);
+      getConversationsList(user.uid);
     },
     (error) => {
       console.log("Login failed with exception:", { error });
@@ -134,29 +200,54 @@ const logUserIn = (authKey, UID) => {
   );
 };
 
-// Función para obtener la lista de grupos del usuario en cuestión
-const getGroupsList = (uid) => {
+// Función para obtener lista de chats
+const getConversationsList = (user) => {
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      onBehalfOf: uid,
+      onBehalfOf: user,
       apikey: "a76fca94f28c87446b3de6dd7785355fbc9d8d78",
     },
   };
 
-  fetch("https://23195116ca7e245f.api-eu.cometchat.io/v3/groups", options)
+  fetch(
+    "https://23195116ca7e245f.api-eu.cometchat.io/v3/conversations?perPage=100&page=1",
+    options
+  )
     .then((response) => response.json())
     .then((response) => {
-      allGroups.value = response;
-      console.log(allGroups);
-      let temp = "";
-      response.data.map((groups) => {
-        temp += `<button class="flex p-3 box h-24 w-[17rem] cursor-pointer" v-on:click="loadGroup('${groups.guid}')">${groups.name}</button>`;
-        document.getElementById("prova").innerHTML = temp;
+      console.log(response);
+      allGroups.value = response.data;
+      response.data.map((chat) => {
+        console.log(chat.conversationWith);
+        getLastMessage(user, chat.conversationWith.guid);
       });
     })
     .catch((err) => console.error(err));
+};
+
+// Obtener el ultimo mensaje de cada conversación
+const getLastMessage = (user, GID) => {
+  const GUID = GID;
+  const limit = 1;
+  const messagesRequest = new CometChat.MessagesRequestBuilder()
+    .setGUID(GUID)
+    .setLimit(limit)
+    .build();
+
+  messagesRequest.fetchPrevious().then((messages) => {
+    messages = messages.filter((message) => message.type === "text");
+    const lastMessage = messages[0].data.text;
+
+    if (lastMessage.length > 19) {
+      document.getElementById(`last-message-${GUID}`).innerHTML =
+        lastMessage.substring(0, 20) + "...";
+    } else {
+      document.getElementById(`last-message-${GUID}`).innerHTML =
+        lastMessage.substring(0, 20);
+    }
+  });
 };
 
 // Función para entrar al grupo y cargar los mensajes del mismo
@@ -208,22 +299,6 @@ const createMsgListener = () => {
   );
 };
 
-// Convertir la hora del formato numerico a fecha
-const convertStringToDate = (strTime) => {
-  const timestamp = Number(strTime) * 1000;
-  const date = new Date(timestamp);
-
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  // const ampm = hours >= 12 ? "pm" : "am";
-  // hours = hours % 12;
-  hours = hours ? hours : 12;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  var timeStr = hours + ":" + minutes;
-
-  return timeStr.toString();
-};
-
 const getMessageDate = (strTime) => {
   const timestamp = Number(strTime) * 1000;
   const date = new Date(timestamp);
@@ -257,9 +332,9 @@ const getMessageDate = (strTime) => {
 };
 
 // Obtener la lista de mensajes de un grupo
-const getMessageHistory = (user, GID) => {
+const getMessageHistory = (user, GID, lim) => {
   const GUID = GID;
-  const limit = 30;
+  const limit = lim ? lim : 100;
   const messagesRequest = new CometChat.MessagesRequestBuilder()
     .setGUID(GUID)
     .setLimit(limit)
@@ -375,5 +450,124 @@ const sendMessage = () => {
       console.log("Error al enviar el mensaje:", error);
     }
   );
+};
+
+// Funcion para llenar valores del modal de crear un chat/grupo
+const newChat = (tipo) => {
+  getUsersList();
+  toggleMenu();
+  toggleModal();
+  chatType.value = tipo;
+  if (tipo === "chat") {
+    chatTag.value = "usuario";
+  } else {
+    chatTag.value = "grupo";
+  }
+};
+
+const createNewChat = () => {
+  // En cas de ser un grup
+  let newChatName;
+  if (chatTag.value === "grupo") {
+    newChatName = document.getElementById("new-chat-input").value;
+  }
+
+  // Variable on guardarem els usuaris seleccionats
+  const select = document.getElementById("new-chat-select");
+
+  var selected = [...select.options]
+    .filter((option) => option.selected)
+    .map((option) => option.value);
+
+  if (selected.length === 1) {
+    newChatName = selected[0] + "_" + userData.name + "_chat";
+  }
+
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      onBehalfOf: userData.uid,
+      "content-type": "application/json",
+      apikey: "a76fca94f28c87446b3de6dd7785355fbc9d8d78",
+    },
+    body: JSON.stringify({
+      members: { participants: selected },
+      type: chatTag.value === "grupo" ? "public" : "private",
+      guid: newChatName,
+      name: newChatName,
+    }),
+  };
+
+  fetch("https://23195116ca7e245f.api-eu.cometchat.io/v3/groups", options)
+    .then((response) => response.json())
+    .then((response) => {
+      toggleModal();
+      getConversationsList(userData.uid);
+    })
+    .catch((err) => console.error(err));
+};
+
+const getUsersList = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      apikey: "a76fca94f28c87446b3de6dd7785355fbc9d8d78",
+    },
+  };
+
+  fetch(
+    "https://23195116ca7e245f.api-eu.cometchat.io/v3/users?perPage=100&page=1",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      let data = response.data;
+      data = data.filter((user) => user.name != userData.name);
+      usersList.value = data;
+    })
+    .catch((err) => console.error(err));
+};
+
+// Funciones Externas al chat
+
+// Convertir la hora del formato numerico a fecha
+const convertStringToDate = (strTime) => {
+  const timestamp = Number(strTime) * 1000;
+  const date = new Date(timestamp);
+
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  // const ampm = hours >= 12 ? "pm" : "am";
+  // hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var timeStr = hours + ":" + minutes;
+
+  return timeStr.toString();
+};
+
+// Hacer visible o no el menú de "Nuevo grupo/chat"
+const toggleMenu = () => {
+  const menu = document.getElementById("new-chat-menu");
+  if (menu.classList.contains("flex")) {
+    menu.classList.remove("flex");
+    menu.classList.add("hidden");
+  } else {
+    menu.classList.remove("hidden");
+    menu.classList.add("flex");
+  }
+};
+
+const toggleModal = () => {
+  const modal = document.getElementById("new-chat-modal");
+  if (modal.classList.contains("flex")) {
+    modal.classList.remove("flex");
+    modal.classList.add("hidden");
+  } else {
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+  }
 };
 </script>
