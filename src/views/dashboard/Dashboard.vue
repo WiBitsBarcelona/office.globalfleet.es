85a5fd1b98b3686ed31a59398f1b8e48bc6ed34b
Dorian Gonzalez <doriandarren1@gmail.com>
2023-05-17 11:50:19 +0200

    ad dashboard

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-12 gap-6 mt-5">
-    <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+    <div class="col-span-12 sm:col-span-6 xl:col-span-4 intro-y">
       <div class="report-box zoom-in">
         <div class="box p-5">
           <div class="flex">
@@ -20,8 +20,54 @@
             <div class="text-base text-slate-500 mt-1">Item Sales</div>
           </div>
         </div>
-      </div>
     </div>
+
+    <div class="col-span-12 sm:col-span-6 xl:col-span-4 intro-y">
+      <div class="report-box zoom-in">
+        <div class="box p-5">
+          <div class="flex">
+            <ShoppingCartIcon class="report-box__icon text-primary" />
+            <div class="ml-auto">
+              <!-- <Tippy 
+                tag="div"
+                          class="report-box__indicator bg-success cursor-pointer"
+                          content="33% Higher than last month"
+                        >
+                          33%
+                          <ChevronUpIcon class="w-4 h-4 ml-0.5" />
+                        </Tippy> -->
+            </div>
+            </div>
+            <div class="text-3xl font-medium leading-8 mt-6">4.710</div>
+            <div class="text-base text-slate-500 mt-1">Item Sales</div>
+          </div>
+        </div>
+    </div>
+
+    <div class="col-span-12 sm:col-span-6 xl:col-span-4 intro-y">
+      <div class="report-box zoom-in">
+        <div class="box p-5">
+          <div class="flex">
+            <ShoppingCartIcon class="report-box__icon text-primary" />
+            <div class="ml-auto">
+              <!-- <Tippy 
+                tag="div"
+                          class="report-box__indicator bg-success cursor-pointer"
+                          content="33% Higher than last month"
+                        >
+                          33%
+                          <ChevronUpIcon class="w-4 h-4 ml-0.5" />
+                        </Tippy> -->
+            </div>
+            </div>
+            <div class="text-3xl font-medium leading-8 mt-6">4.710</div>
+            <div class="text-base text-slate-500 mt-1">Item Sales</div>
+          </div>
+        </div>
+    </div>
+    
+  </div>
+    
 </template>
 
 <script setup>
