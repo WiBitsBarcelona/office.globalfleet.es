53909ba45989277d00cf2e570826c98fc738c91b
Dorian Gonzalez <doriandarren1@gmail.com>
2023-06-15 17:54:43 +0200

    enhance dashboard resumen trips

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -5,16 +5,16 @@
       <div class="col-span-12 mt-8">
         <div class="intro-y flex items-center h-10">
           <h2 class="text-lg font-medium truncate mr-5">{{ $t("dashboard.title") }}</h2>
-          <!-- <a href="" class="ml-auto flex items-center text-primary dark:text-light">
+          <a href="" class="ml-auto flex items-center text-primary dark:text-light">
             <RefreshCcwIcon class="w-4 h-4 mr-3" /> <span class="">{{ $t("dashboard.reload") }}</span>
-          </a> -->
+          </a>
         </div>
         <div class="grid grid-cols-12 gap-6 mt-5">
           <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
             <div class="report-box zoom-in">
               <div class="box p-5">
                 <div class="flex">
-                  <NavigationIcon class="report-box__icon text-primary" />
+                  <NavigationIcon class="report-box__icon text-gray-500" />
 
                 </div>
                 <div class="text-3xl font-medium leading-8 mt-6">{{ trip_created_nb }}</div>
@@ -26,7 +26,7 @@
             <div class="report-box zoom-in">
               <div class="box p-5">
                 <div class="flex">
-                  <AlertTriangleIcon class="report-box__icon text-pending" />
+                  <NavigationIcon class="report-box__icon text-orange-300" />
                 </div>
                 <div class="text-3xl font-medium leading-8 mt-6">{{ trip_pending_nb }}</div>
                 <div class="text-base text-slate-500 mt-1">{{ $t("trip_pending") }}</div>
@@ -37,7 +37,7 @@
             <div class="report-box zoom-in">
               <div class="box p-5">
                 <div class="flex">
-                  <MessageCircleIcon class="report-box__icon text-primary" />
+                  <NavigationIcon class="report-box__icon text-blue-300" />
                 </div>
                 <div class="text-3xl font-medium leading-8 mt-6">{{ trip_progress_nb }}</div>
                 <div class="text-base text-slate-500 mt-1">
@@ -50,7 +50,7 @@
             <div class="report-box zoom-in">
               <div class="box p-5">
                 <div class="flex">
-                  <FileTextIcon class="report-box__icon text-primary" />
+                  <NavigationIcon class="report-box__icon text-green-300" />
                 </div>
                 <div class="text-3xl font-medium leading-8 mt-6">{{ trip_completed_nb }}</div>
                 <div class="text-base text-slate-500 mt-1">
@@ -104,6 +104,8 @@
   const trip_pending_nb = ref('');
   const trip_progress_nb = ref('');
 
+  
+
 
 
   onMounted(async() => {
@@ -116,6 +118,8 @@
     trip_created_nb.value = dashboard.value.trip_created_nb;
     trip_pending_nb.value = dashboard.value.trip_pending_nb;
     trip_progress_nb.value = dashboard.value.trip_progress_nb;
+
+
 
   });
 
