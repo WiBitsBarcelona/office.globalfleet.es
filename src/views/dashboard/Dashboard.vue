fc71c04539355e4f7a96c403de723f6cd0f33803
Dorian Gonzalez <doriandarren1@gmail.com>
2023-06-16 10:28:12 +0200

    Merge Dorian

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -62,45 +62,6 @@
         </div>
       </div>
 
-      <!-- BEGIN: Trips Report -->
-      <div class="col-span-8 sm:col-span-6 lg:col-span-3 mt-6">
-        <div class="intro-y flex items-center h-10">
-          <h2 class="text-lg font-medium truncate mr-5">{{ $t("dashboard.trips_graph_title") }}</h2>
-          <a href="/trips" class="ml-auto text-primary truncate dark:text-light">{{ $t("dashboard.show_more") }}</a>
-        </div>
-        <div class="intro-y box p-5 mt-5">
-          <div class="text-right">
-            <span class="text-2xl text-primary mr-1 dark:text-light">5</span> {{ $t("dashboard.trips") }}
-          </div>
-          <withDirectives>
-            <ReportPieChart :height="213" />
-          </withDirectives>
-          <div class="w-52 sm:w-auto mx-auto mt-8">
-            <div class="flex items-center">
-              <div class="w-2 h-2 bg-primary rounded-full mr-3"></div>
-              <span class="truncate">{{ $t("dashboard.current_trips") }} </span>
-              <span class="font-medium ml-auto">2</span>
-            </div>
-            <div class="flex items-center mt-4">
-              <div class="w-2 h-2 bg-success rounded-full mr-3"></div>
-              <span class="truncate">{{ $t("dashboard.finished_trips") }}</span>
-              <span class="font-medium ml-auto">1</span>
-            </div>
-            <div class="flex items-center mt-4">
-              <div class="w-2 h-2 bg-warning rounded-full mr-3"></div>
-              <span class="truncate">{{ $t("dashboard.pending_trips") }}</span>
-              <span class="font-medium ml-auto">1</span>
-            </div>
-            <div class="flex items-center mt-4">
-              <div class="w-2 h-2 bg-light rounded-full mr-3"></div>
-              <span class="truncate">{{ $t("dashboard.graph_new_trips") }}</span>
-              <span class="font-medium ml-auto">1</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- END: Trips Report -->
-
       <!-- BEGIN: Vehicles Map -->
       <div class="col-span-12 xl:col-span-12 mt-6">
         <div class="intro-y block sm:flex items-center">
@@ -143,21 +104,48 @@
 </template>
 
 <script setup>
-import ReportPieChart from "@/components/report-pie-chart/Main.vue";
+import { computed, ref, onMounted } from 'vue';
 import VehiclesMap from "@/components/vehicles-map/Main.vue";
+import useVehicles from "@/composables/vehicles";
+import useDashboard from '@/composables/dashboard.js';
+
+
+const {dashboard, getDashboard} = useDashboard();
+
+
+  const trip_completed_nb = ref('');
+  const trip_created_nb = ref('');
+  const trip_pending_nb = ref('');
+  const trip_progress_nb = ref('');
+
+
+
+const select = ref("");
+
+const { vehicles, getVehicles } = useVehicles();
+
+const totalVehicles = ref(0);
+
+const findData = async () => {
+  await getVehicles();
+  console.log({ ...vehicles });
+  totalVehicles.value = computed(() => vehicles.value.length);
+  console.log(totalVehicles.value);
+  
+}
 
 
 
 
   onMounted(async() => {
     await getDashboard();
-    //console.log("objjj", dashboard);
+
+    findData();
     
     trip_completed_nb.value = dashboard.value.trip_completed_nb;
     trip_created_nb.value = dashboard.value.trip_created_nb;
     trip_pending_nb.value = dashboard.value.trip_pending_nb;
     trip_progress_nb.value = dashboard.value.trip_progress_nb;
-
 
 
   });
