b8bc3ef14ae795dff948ed120cd788a7d945a325
Dorian Gonzalez <doriandarren1@gmail.com>
2023-07-19 14:53:55 +0200

    fix refresh image map

--- src/components/dashboard-map/Main.vue
+++ src/components/dashboard-map/Main.vue
@@ -714,10 +714,10 @@ const init = async (initializeMap) => {
                   </div>
                 </div>
               </div>`;
-              
+
       //TODO aquii
       if (speed >= 5) {
-        //markerIcon = "./src/assets/images/markers/map-marker-green.svg";
+        //markerIcon = "/src/assets/images/markers/map-marker-green.svg";
         markerIcon = imageAssets["/src/assets/images/markers/map-marker-green.svg"].default;
       } else {
         //markerIcon = "/src/assets/images/markers/map-marker-red.svg";
@@ -807,7 +807,8 @@ function createCenterControl(map) {
   const controlButton = document.createElement("button");
   // Set CSS for the control.
   controlButton.style.backgroundColor = "#fff";
-  controlButton.style.backgroundImage = "url('/src/assets/images/refresh.png')";
+  controlButton.style.backgroundImage = "url('" + imageAssets["/src/assets/images/markers/refresh.png"].default + "')";
+  //controlButton.style.backgroundImage = "url('/src/assets/images/refresh.png')";
   //controlButton.style.backgroundImage = "url(imageAssets['/src/assets/images/map-marker-region-dark.svg'])";
   controlButton.style.backgroundSize = "cover"
   controlButton.style.width = "40px";
