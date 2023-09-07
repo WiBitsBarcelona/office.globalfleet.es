65616b5384157c25fd16feeff4bfd21092ea304f
Artur Torrelles <artur@globaltank.eu>
2023-09-07 15:35:10 +0200

    Components Folders Update

---
+++ src/components/stages/StageDetails.vue
@@ -0,0 +1,27 @@
+<template>
+    <p>Stages</p>
+    <ul>
+        <li>
+            
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import { ref, watchEffect } from 'vue';
+
+const props = defineProps([
+  'stages'
+]);
+
+const stages = ref('');
+
+watchEffect(() => {
+    stages.value = props.stages;
+
+    console.log(stages.value);
+});
+
+
+
+</script>
