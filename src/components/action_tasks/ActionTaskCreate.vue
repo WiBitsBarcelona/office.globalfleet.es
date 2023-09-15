15e6144713538f70d3031d4ac14345bd441ae6be
Dorian Gonzalez <doriandarren1@gmail.com>
2023-09-15 14:20:28 +0200

    rm button trip

--- src/components/action_tasks/ActionTaskCreate.vue
+++ src/components/action_tasks/ActionTaskCreate.vue
@@ -1,5 +1,4 @@
 <template>
-
 	<!-- BEGIN: Form -->
 	<form class="validate-form" @submit.prevent="save">
 
@@ -7,87 +6,55 @@
 		<div class="grid grid-cols-12 gap-6">
 
 			<div class="col-span-12 md:col-span-5 lg:col-span-5">
-                <div class="input-form">
-                    <label for="action_type_id" class="form-label w-full">
-                        {{ $t("action_type") }}
-                    </label>
+				<div class="input-form">
+					<label for="action_type_id" class="form-label w-full">
+						{{ $t("action_type") }}
+					</label>
 
-                    <select 
-                        v-model.trim="validate.action_type_id.$model" 
-                        id="action_type_id"          
-                        name="action_type_id"
-                        class="form-control" 
-                        :class="{ 'border-danger': validate.action_type_id.$error }">
+					<select v-model.trim="validate.action_type_id.$model" id="action_type_id" name="action_type_id"
+						class="form-control" :class="{ 'border-danger': validate.action_type_id.$error }">
 
-                        <option value="" selected>Seleccione</option>
-                        <option v-for="item in selectActionTypes" :value="item.id">
-                            {{ item.name }}
-                        </option>
+						<option value="" selected>Seleccione</option>
+						<option v-for="item in selectActionTypes" :value="item.id">
+							{{ item.name }}
+						</option>
 
-                    </select>
+					</select>
 
 
 
-                    <template v-if="validate.action_type_id.$error">
-                        <div v-for="(error, index) in validate.action_type_id.$errors" :key="index"
-                            class="text-danger mt-2">
-                            {{ error.$message }}
-                        </div>
-                    </template>
-                </div>
-            </div>
+					<template v-if="validate.action_type_id.$error">
+						<div v-for="(error, index) in validate.action_type_id.$errors" :key="index"
+							class="text-danger mt-2">
+							{{ error.$message }}
+						</div>
+					</template>
+				</div>
+			</div>
 
 
 			<div class="col-span-12 md:col-span-5 lg:col-span-5">
-                <div class="input-form">
-                    <label for="action_type_model" class="form-label w-full">
-                        {{ $t("action_type") }}
-                    </label>
-
-                    <select 
-                        v-model.trim="validate.action_type_model.$model" 
-                        id="action_type_model"          
-                        name="action_type_model"
-                        class="form-control" 
-                        :class="{ 'border-danger': validate.action_type_model.$error }"
-						@change="onChangeSelectActionModel($event)"
-					>
-
-                        <option value="" selected>Seleccione</option>
-                        <option v-for="item in selectActionTypeModel" :value="item.id">
-                            {{ item.name }}
-                        </option>
-
-                    </select>
-
-
-
-                    <template v-if="validate.action_type_model.$error">
-                        <div v-for="(error, index) in validate.action_type_model.$errors" :key="index"
-                            class="text-danger mt-2">
-                            {{ error.$message }}
-                        </div>
-                    </template>
-                </div>
-            </div>
-
-
-
-			<div class="col-span-12 md:col-span-2 lg:col-span-">
 				<div class="input-form">
-					<label for="order_number" class="form-label w-full">
-						{{ $t("order_number") }}
+					<label for="action_type_model" class="form-label w-full">
+						{{ $t("action_type") }}
 					</label>
-					<input
-						v-model.trim="validate.order_number.$model"
-						id="order_number"
-						type="text"
-						name="order_number"
-						class="form-control"
-						:class="{ 'border-danger': validate.order_number.$error }"
-					/>
-					<template v-if="validate.order_number.$error">
-						<div v-for="(error, index) in validate.order_number.$errors" :key="index" class="text-danger mt-2">
+
+					<select v-model.trim="validate.action_type_model.$model" id="action_type_model" name="action_type_model"
+						class="form-control" :class="{ 'border-danger': validate.action_type_model.$error }"
+						@change="onChangeSelectActionModel($event)">
+
+						<option value="" selected>Seleccione</option>
+						<option v-for="item in selectActionTypeModel" :value="item.id">
+							{{ item.name }}
+						</option>
+
+					</select>
+
+
+
+					<template v-if="validate.action_type_model.$error">
+						<div v-for="(error, index) in validate.action_type_model.$errors" :key="index"
+							class="text-danger mt-2">
 							{{ error.$message }}
 						</div>
 					</template>
@@ -96,37 +63,48 @@
 
 
 
-			<div class="col-span-12 md:col-span-5 lg:col-span-5">
-                <div class="input-form">
-                    <label for="action_form_field" class="form-label w-full">
-                        {{ $t("action_form_field") }}
-                    </label>
+			<div class="col-span-12 md:col-span-2 lg:col-span-2">
+				<div class="input-form">
+					<label for="order_number" class="form-label w-full">
+						{{ $t("order_number") }}
+					</label>
+					<input v-model.trim="validate.order_number.$model" id="order_number" type="text" name="order_number"
+						class="form-control" :class="{ 'border-danger': validate.order_number.$error }" />
+					<template v-if="validate.order_number.$error">
+						<div v-for="(error, index) in validate.order_number.$errors" :key="index" class="text-danger mt-2">
+							{{ error.$message }}
+						</div>
+					</template>
+				</div>
+			</div>
 
-                    <select 
-                        v-model.trim="validate.action_form_field.$model" 
-                        id="action_form_field"          
-                        name="action_form_field"
-                        class="form-control" 
-                        :class="{ 'border-danger': validate.action_form_field.$error }"
-					>
+			<div class="col-span-12 md:col-span-5 lg:col-span-5" v-if="isShowActionFormField">
+				<div class="input-form">
+					<label for="action_form_field_id" class="form-label w-full">
+						{{ $t("action_form_field") }}
+					</label>
 
-                        <option value="" selected>Seleccione</option>
-                        <option v-for="item in selectActionFormFields" :value="item.id">
-                            {{ item.name }}
-                        </option>
+					<select v-model.trim="validate.action_form_field_id.$model" id="action_form_field" name="action_form_field"
+						class="form-control" :class="{ 'border-danger': validate.action_form_field_id.$error }">
 
-                    </select>
+						<option value="" selected>Seleccione</option>
+						<option v-for="item in selectActionFormFields" :value="item.id">
+							{{ item.name }}
+						</option>
+
+					</select>
 
 
 
-                    <template v-if="validate.action_form_field.$error">
-                        <div v-for="(error, index) in validate.action_form_field.$errors" :key="index"
-                            class="text-danger mt-2">
-                            {{ error.$message }}
-                        </div>
-                    </template>
-                </div>
-            </div>
+					<template v-if="validate.action_form_field_id.$error">
+						<div v-for="(error, index) in validate.action_form_field_id.$errors" :key="index"
+							class="text-danger mt-2">
+							{{ error.$message }}
+						</div>
+					</template>
+				</div>
+			</div>
+
 
 
 			<div class="col-span-12 md:col-span-12 lg:col-span-12">
@@ -134,8 +112,9 @@
 					<label for="description" class="form-label w-full">
 						{{ $t("description") }}
 					</label>
-					
-                    <textarea v-model.trim="validate.description.$model" id="description" name="description" class="form-control">
+
+					<textarea v-model.trim="validate.description.$model" id="description" name="description"
+						class="form-control">
                     </textarea>
 
 					<template v-if="validate.description.$error">
@@ -165,124 +144,135 @@
 
 	</form>
 	<!-- END: Form -->
-
 </template>
 <script setup>
 
-	import { onMounted, reactive, toRefs, ref } from 'vue';
-	//import useActionsTasks from '@/composables/action_tasks';
-	import useActionType from '@/composables/action_types';
-	import useActionFormField from '@/composables/action_form_fields.js';
-	import { required, minLength, maxLength, email, url, integer } from '@vuelidate/validators';
-	import { useVuelidate } from '@vuelidate/core';
-	import { helpers } from '@vuelidate/validators';
-	import { useI18n } from 'vue-i18n';
+import { onMounted, reactive, toRefs, ref } from 'vue';
+//import useActionsTasks from '@/composables/action_tasks';
+import useActionType from '@/composables/action_types';
+import useActionFormField from '@/composables/action_form_fields.js';
+import { required, minLength, maxLength, email, url, integer } from '@vuelidate/validators';
+import { useVuelidate } from '@vuelidate/core';
+import { helpers } from '@vuelidate/validators';
+import { useI18n } from 'vue-i18n';
+import Swal from "sweetalert2";
 
-	const { t } = useI18n();
-	const { actionTypes, getActionTypes } = useActionType();
-	const { actionFormFields, getActionFormFields } = useActionFormField();
-	
-	const emit = defineEmits(['cancelActionTaskForm', 'addActionTaskForm']);
-	const props = defineProps(['stageIndex', 'taskIndex']);
+const { t } = useI18n();
+const { actionTypes, getActionTypes } = useActionType();
+const { actionFormFields, getActionFormFields } = useActionFormField();
 
-
-	const CAMERA_ID = 1;
-	const SCANNER_ID = 2;
-	const FORM_ID = 3;
+const emit = defineEmits(['cancelActionTaskForm', 'addActionTaskForm']);
+const props = defineProps(['stageIndex', 'taskIndex']);
 
 
-
-	const selectActionTypes = ref([]);
-	const selectActionTypeModel = ref([]);
-
-
-	const selectActionFormFields = ref([]);
+const CAMERA_ID = 1;
+const SCANNER_ID = 2;
+const FORM_ID = 3;
 
 
 
-	
+const selectActionTypes = ref([]);
+const selectActionTypeModel = ref([
+	{ id: CAMERA_ID, name: 'Camara' },
+	{ id: SCANNER_ID, name: 'Escaner' },
+	{ id: FORM_ID, name: 'Formulario' },
+]);
 
 
+const selectActionFormFields = ref([]);
+
+const isShowActionFormField = ref(false);
 
 
+const rules = {
+	action_type_id: {
+		required: helpers.withMessage(t("form.required"), required),
+	},
+	order_number: {
+		required: helpers.withMessage(t("form.required"), required),
+	},
+	action_type_model: {
+		required: helpers.withMessage(t("form.required"), required),
+	},
+	description: {
+	},
+	action_form_field_id: {
+	},
+};
 
-	const rules = {
-		action_type_id: {
-			required: helpers.withMessage(t("form.required"), required),
-		},
-		order_number: {
-			required: helpers.withMessage(t("form.required"), required),
-		},
-		action_type_model: {
-			required: helpers.withMessage(t("form.required"), required),
-		},
-		description: {
-		},
-		action_form_field: {
-		},
-	};
+const formData = reactive({
+	action_type_id: "",
+	action_type_model: "",
+	action_form_field_id: "",
+	action_form_field_name: "",
+	action_type_name: "",
+	order_number: "1",
+	description: "Descripcion Action Tasks",
+});
 
-	const formData = reactive({
-		action_type_id: "",
-		action_type_model: "",
-		action_form_field: "",
-		action_type_name: "",
-		order_number: "1",
-		description: "Descripcion Action Tasks",
-	});
+const validate = useVuelidate(rules, toRefs(formData));
 
-	const validate = useVuelidate(rules, toRefs(formData));
+const save = () => {
 
-	const save = () => {
+	validate.value.$touch();
+	if (validate.value.$invalid) {
+		//TODO
+	} else {
 
-		validate.value.$touch();
-		if (validate.value.$invalid) {
-			//TODO
-		} else {
 
-			//Find element action
-			const selectedAction = selectActionTypes.value.find(elem => elem.id === formData.action_type_id);
-			formData.action_type_name = selectedAction.name;
+		//TODO 
+		if (parseInt(formData.action_type_model) === FORM_ID) {
+			if(formData.action_form_field_id === '' || formData.action_form_field_id === undefined){
+				//TODO alert
 
-			emit('addActionTaskForm', props.stageIndex, props.taskIndex, {...formData});
+				Swal.fire({
+					icon: 'warning',
+					title: t("Complete el campo del formulario"),
+					showCancelButton: false,
+					confirmButtonText: t("message.ok"),
+					confirmButtonColor: import.meta.env.VITE_SWEETALERT_COLOR_BTN_SUCCESS,
+				});
+
+				return;
+			}
+
 		}
-	};
+
+		//Find element action
+		const selectedAction = selectActionTypes.value.find(elem => elem.id === formData.action_type_id);
+		formData.action_type_name = selectedAction.name;
+
+		emit('addActionTaskForm', props.stageIndex, props.taskIndex, { ...formData });
+	}
+};
 
 
-	const onChangeSelectActionModel = (event) => {
-		console.log(event.target.value);
+const onChangeSelectActionModel = (event) => {
+	console.log(event.target.value);
 
-		if(event.target.value === FORM_ID){
+	if (parseInt(event.target.value) === FORM_ID) {
 
+		isShowActionFormField.value = true;
 
+	} else {
 
-		}
+		isShowActionFormField.value = false;
 
 	}
 
-
-
-
-	onMounted(async () => {
-		
-		await getActionTypes();
-		selectActionTypes.value = actionTypes.value;
-
-
-		selectActionTypeModel.value = [
-			{id:CAMERA_ID, name:'Camara'},
-			{id:SCANNER_ID, name:'Escaner'},
-			{id:FORM_ID, name:'Formulario'},
-		];
-
-
-
-		await getActionFormFields();
-		selectActionFormFields.value = actionFormFields.value;
+}
 
 
 
 
-	});
+onMounted(async () => {
+
+	await getActionTypes();
+	selectActionTypes.value = actionTypes.value;
+
+	await getActionFormFields();
+	selectActionFormFields.value = actionFormFields.value;
+
+});
 
 </script>
