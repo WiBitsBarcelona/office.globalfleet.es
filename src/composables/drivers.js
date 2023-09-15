52e7c69dde5ee89de2ee18e158df27d29cfd95ab
Dorian Gonzalez <doriandarren1@gmail.com>
2023-09-15 09:50:01 +0200

    enhance driver

--- src/composables/drivers.js
+++ src/composables/drivers.js
@@ -47,10 +47,8 @@ export default function useDriver() {
 	const storeDriver = async (data) => {
 		errors.value = '';
 
-		console.log(data);
 		try {
 			await axios.post(`${import.meta.env.VITE_API_URL_GLOBALFLEET}drivers/store`, data, config);
-			//await router.push({ name: 'driver.index' });
 		} catch (e) {
 			console.log(e);
 			// if (e.response.status_code === 422) {
